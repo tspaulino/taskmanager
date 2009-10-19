f3f9a462936efae3ef98dbb2a6ee0cb6addf2fdb
Rodolfo Carvalho <rodolfo@rcarvalho.eti.br>
2009-10-19 12:16:15 -0200

    Authentication and Authorization gems
    Added authlogic and acl9 gems to config.gem session on config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "binarylogic-authlogic", :lib => "authlogic", :source => "http://gems.github.com"
+  config.gem "be9-acl9", :lib => "acl9", :source => "http://gems.github.com"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
