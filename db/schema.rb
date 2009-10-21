c8fee8f3ae21d9f4b876f89af3256419b573194c
Tiago dos Santos Paulino <tiago@tiago-laptop.(none)>
2009-10-21 06:52:08 -0200

    Project model created. Initial test made, but need to add tests with users roles.
      Relationship:
        - Project belongs and has many users:
            $ Project belongs to a owner (User);
            $ Project has many developers (Users).
        - Need to set a Factory for Test this relationship.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091020200212) do
+ActiveRecord::Schema.define(:version => 20091020232758) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
